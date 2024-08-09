10afd36be6547d12571b6c21e1804c062a2a6466
D-Agbikossi <d.agbikossi@alustudent.com>
2024-08-09 09:11:58 -0700

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-""" Class that inherits the attributes references of class list
-
-    Args:
-        list: class list
-
-    """
+"""Class that inherits from the built-in list class."""
 class MyList(list):
     """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """ Method that prints the sorted list """
+        """Prints the list in a sorted order."""
         print(sorted(self))
