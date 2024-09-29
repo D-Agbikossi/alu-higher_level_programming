4a0f00c64ae42fa88a94bfaa2a92d7119cc38f0e
D-Agbikossi <d.agbikossi@alustudent.com>
2024-09-29 11:51:16 -0700

    Concatenate

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,8 +2,8 @@
 
 if (!process.argv[2]) {
   console.log('undefined is undefined');
-} else if (!progress.argv[3]) {
-  console.log(process.argv[2] + 'is undefined');
+} else if (!process.argv[3]) {
+  console.log(process.argv[2] + ' is undefined');
 } else {
-  console.log(process.argv[2] + 'is' + process.argv[3]);
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
